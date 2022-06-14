aaa57b18a280a89d1ae0eccefbf5e077686bcba2
Hanieh Navab <107445808+Hani-navab@users.noreply.github.com>
2022-06-14 12:21:10 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TreeViewDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tvw = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // tvw
+            // 
+            this.tvw.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tvw.Location = new System.Drawing.Point(0, 0);
+            this.tvw.Name = "tvw";
+            this.tvw.Size = new System.Drawing.Size(349, 342);
+            this.tvw.TabIndex = 0;
+            this.tvw.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tvw_AfterSelect);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 342);
+            this.Controls.Add(this.tvw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvw;
+    }
+}
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace TreeViewDemo
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dbg = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dbg)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dbg
+            // 
+            this.dbg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dbg.Location = new System.Drawing.Point(0, 0);
+            this.dbg.Name = "dbg";
+            this.dbg.Size = new System.Drawing.Size(292, 273);
+            this.dbg.TabIndex = 0;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.dbg);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.dbg)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dbg;
+    }
+}
